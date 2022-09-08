d27e75ea90621c33877763bf4eac7a814514529d
root <beklibya@gmail.com>
2022-09-08 14:58:49 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,23 @@
 
 #include <stdio.h>
 
+/*printf*/
 
+
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
+
+isx?1
+clude <stdio.h>
+#/printf #/
 
 
 

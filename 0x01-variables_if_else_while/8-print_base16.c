b73f56f65cb17300b98803588f323f6c4e28a08c
root <beklibya@gmail.com>
2022-09-12 13:07:40 -0700

    5

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0 ; c <= 15 ; c++)
+	{
+		putchar(c + '0');
+	}
+	putchar('\n');
+	return (0);
+}

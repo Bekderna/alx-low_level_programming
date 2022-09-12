297cdd166323a2bcf3551c2db7d2b99993580dc3
root <beklibya@gmail.com>
2022-09-12 13:42:05 -0700

    5

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0 ; c <= 9 ; c++)
+	{
+		putchar(c + '0');
+		putchar(' ');
+		putchar(',')
+	}
+	putchar('\n');
+	return (0);
+}

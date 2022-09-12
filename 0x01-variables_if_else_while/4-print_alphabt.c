017e1af2be138a37fdb495a8c008a62675020441
root <beklibya@gmail.com>
2022-09-12 12:50:00 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z0'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+			continue;
+			else
+			{
+			putchar(ch);
+			}
+	}
+	putchar('\n');
+	return (0);
+}

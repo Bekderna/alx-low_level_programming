3f6ebdabc80f20beeabfbbbf5baea397f6d41574
root <beklibya@gmail.com>
2022-09-12 14:21:27 -0700

    103

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+	int ch;
+	int d;
+	int a;
+
+	for (a = 0; a <= 9 ; a++)
+	{
+	for (d = 0 ; d <= 9; d++)
+		{
+	for (c = 0 ; c <= 9 ; c++)
+			{
+	for (ch = 0 ; ch <= 9 ; ch++)
+				{
+		putchar(a + '0');
+		putchar(d + '0');
+		putchar(' ');
+		putchar(c + '0');
+		putchar(ch + '0');
+		if (c == 9 && a == 9 && d == 9 && ch == 9)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}

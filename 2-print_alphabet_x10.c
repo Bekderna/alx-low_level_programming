94e963ed66f52f5f2c4cdea0f7837c4e47a63a64
bekderna <beklibya@gmail.com>
2022-09-14 12:52:04 -0700

    0

---
+++ 2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   * Description: 'the program's description'
+ *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+		  print_alphabet();
+	return (0);
+}
+	void print_alphabet(void)
+	{
+		for (ch = 'a' ; ch = 'z' ; ch++)
+		{
+			putchar(ch);
+		}
+		putchar ('\n');
+	}
+}
+

d86ff851624bfd849c041f6e56c5e545143c1ce4
bekderna <beklibya@gmail.com>
2022-09-16 15:10:22 -0700

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,15 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * tper case
  * return int
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }

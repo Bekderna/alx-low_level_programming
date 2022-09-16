c4658d912e7a65979601480b4a8f54b7b6b5a0a2
bekderna <beklibya@gmail.com>
2022-09-16 15:54:27 -0700

    5

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * print_numbers - print 01234564789\n
+ * return: 0
+ */
+
+void print_numbers(void)
+{
+	for (i = 0; i <= 9, i++)
+	{
+		putchar('i' + '0');
+	}
+	putchar ('\n');
+	return (0);
+}
+

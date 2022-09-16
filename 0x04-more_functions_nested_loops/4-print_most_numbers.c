10ae3e828b7282ab8c04c4c7394df5c24b439790
bekderna <beklibya@gmail.com>
2022-09-16 16:17:45 -0700

    22

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  * print_numbers - prints 0123456789\n
+ *   *
+ *    * Return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c == '2' || c == '4')
+			continue;
+		else
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+

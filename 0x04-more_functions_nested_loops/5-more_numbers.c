cabb4fce267fda1baf5ff1920215885deb21bd88
bekderna <beklibya@gmail.com>
2022-09-17 13:54:23 -0700

    k

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,14 @@ void more_numbers(void)
 {
 	int x, i;
 
-	for (i = 0; c < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (x = 0; x <= 14; x++)
 		{
 			if (x >= 10)
+			{
 				_putchar('1');
+			}
 			_putchar(x % 10 + '0');
 		}
 		_putchar('\n');

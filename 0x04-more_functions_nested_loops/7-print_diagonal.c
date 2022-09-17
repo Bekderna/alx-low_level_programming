2a48802c13d1fe73136cc7c01eb35fa46cfa298d
bekderna <beklibya@gmail.com>
2022-09-17 14:17:22 -0700

    k

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  * print_line - prints 0123456789\n
+ *   *@n: num
+ *    * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int x, i;
+
+	for (x = 0 ; x < n; x++)
+	{
+		for (i = 0; i < x; i++)
+		_putchar(' ');
+		_putchar('\');
+	}
+	_putchar('\n');
+}

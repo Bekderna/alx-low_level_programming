35bbced3e08e9afc24560b04801f19b83cff4a7e
bekderna <beklibya@gmail.com>
2022-09-19 13:11:02 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  _puts - check the code
+ *   @str : str
+ *    * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(i);
+	}
+}

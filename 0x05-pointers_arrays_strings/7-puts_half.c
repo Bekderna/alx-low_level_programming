ae9ef01d05fce048c2e23bda8aba6b640893141d
bekderna <beklibya@gmail.com>
2022-09-19 14:44:09 -0700

    k

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ *  puts-half- check the code
+ *   @str : str
+ *    * Return: void
+ */
+void puts_half(char *str);
+{
+	int i, x;
+
+	x = strlen(str);
+	for (i = x / 2; i != '\0'  ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}

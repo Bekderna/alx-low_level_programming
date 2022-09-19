b08a6c0e5bb6eca3b736a443d8c7b51ef79c02f9
bekderna <beklibya@gmail.com>
2022-09-19 15:20:37 -0700

    k

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ *  _strcpy- check the code
+ *   @src : str
+ *   @dest : str
+ *    * Return: void
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, x;
+
+	x = strlen(src);
+	for (i = 0; i <= x; i++)
+	{
+		dest[i] = src[i]
+	}
+}

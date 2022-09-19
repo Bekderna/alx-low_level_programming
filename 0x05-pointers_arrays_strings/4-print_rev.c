4e4188387d634640341cc86139ffa83e2b8e0727
bekderna <beklibya@gmail.com>
2022-09-19 13:36:19 -0700

    3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  print_rev- check the code
+ *   @s : str
+ *    * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

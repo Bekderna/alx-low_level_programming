bf49af76b2b819ed1a61904c74b4dc8f9d360bda
bekderna <beklibya@gmail.com>
2022-09-20 13:42:05 -0700

    5

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,24 @@
-#include <time.h>
+#include <main.h>
 #include <stdio.h>
 /**
- *  print_alphabet - prints alphabet
+ *  jack_bauer - prints alphabet
  *    * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void jack_bauer(void)
 {
-	char ch;
+	int i, j;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (i = 0; i <= 23; i++)
 	{
-		putchar(ch);
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar ('\n');
+		}
 	}
-		putchar ('\n');
 }
 

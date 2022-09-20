8b5a5afdf6c68294afcf8cc6e90677cdfc8a2dd3
bekderna <beklibya@gmail.com>
2022-09-20 12:47:32 -0700

    k

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * print_sign- prints alphabet
+ *  @n: num
+ *    * Return: int  (Success)
+ */
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (c > 0)
+	{
+		putchar('-');
+		return ("-1");
+	}
+	else
+	{
+		putchar('0');
+		return (0);
+	}
+}

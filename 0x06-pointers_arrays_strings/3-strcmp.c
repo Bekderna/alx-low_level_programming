e032dd9509539734db81fc682a862dd476099562
bekderna <beklibya@gmail.com>
2022-09-21 12:43:35 -0700

    2

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *  _strcmp - check the code
+ *   @s1 : str
+ *   @s2 : str
+ *
+ *    * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	j = 0;
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] > s2[i])
+			j++;
+		else if (s1[i] < s2[i])
+			j--;
+		else
+			continue;
+	}
+
+	return (j);
+}

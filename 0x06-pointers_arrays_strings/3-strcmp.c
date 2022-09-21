176ddbdad844dcad7d0e7ef35193e36430800ebe
bekderna <beklibya@gmail.com>
2022-09-21 12:55:55 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,18 +8,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, j;
 
 	j = 0;
 	for (i = 0; s1[i] != '\0'; i++)
 	{
-		if (s1[i] > s2[i])
-			return (15);
-		else if (s1[i] < s2[i])
-			return (-15);
+		if (s1[i] != s2[i])
+		{
+			j = s1[i] - s2[i];
+			break;
+		}
 		else
 			continue;
 	}
-
-	return (0);
+	return (j);
 }

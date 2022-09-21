e615867ec685011c205cee51db6074acb3006522
bekderna <beklibya@gmail.com>
2022-09-21 14:02:38 -0700

    5

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,11 +9,12 @@ char *cap_string(char *c)
 {
 	int i;
 
+	if (c[0] >= 'a' && c[0] <= 'z')
+		c[0] = c[0] - 32;
 	for (i = 1; c[i] != '\0'; i++)
 	{
 		if (c[i] >= 'a' && c[i] <= 'z' && c[i - 1] == ' ')
 			c[i] = c[i] - 32;
-
 	}
 	return (c);
 }

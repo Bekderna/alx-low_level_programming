df332e999f4cabb4c1ed6fe89f62fc875c754c04
bekderna <beklibya@gmail.com>
2022-09-21 13:44:46 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *   string_toupper - check the code
+ *   @c : str
+ *
+ *    * Return: int
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && s1[i] <= 'z')
+			c[i] = c[i] - 32;
+
+	}
+	return (c);
+}

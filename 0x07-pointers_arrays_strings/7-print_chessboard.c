b79dca296489da18e249807cc3aebe421508a2a4
bekderna <beklibya@gmail.com>
2022-09-27 14:09:35 -0700

    new

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  *  print_chessboard- check the code
+ *    *   @a : str
+ *       *    * Return: void
+ */
+void print_chessboard(char b(*a)[8])
+{
+	unsigned int i, j;
+
+	for (i = 0; i < a '; i++)
+	{
+		for (j = 0; j < 8; j++)
+			printf (b[i][j]);
+		printf('\n')
+	}
+	return (0);
+}
+

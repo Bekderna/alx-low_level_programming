6918dd8af6604906bf98b34329faee62cb116d6c
bekderna <beklibya@gmail.com>
2022-09-27 13:36:50 -0700

    new

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,15 +12,15 @@ char *_strstr(char *haystack, char *needle)
 	unsigned int i, j, x;
 
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		x = 0
-		for (j = 0; accept[j] != '\0'; j++)
+		x = 0;
+		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i] == needle[j])
 				x++;
 		}
-		if (x = (j - 1)
+		if (x == (j - 1))
 				return (haystack + i - x);
 	}
 	return (0);

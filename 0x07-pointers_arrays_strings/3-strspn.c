5755a5c9a627c7d39663900fb899b9dedc6cba4f
bekderna <beklibya@gmail.com>
2022-09-27 12:32:09 -0700

    new

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  *  *_strspn- check the code
+ *   *   @accept : str
+ *    *   @s : str
+ *      *
+ *       *    * Return: void
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, d;
+
+	d = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+			if accept[j] == s[i]
+				d++;
+	}
+		return (d);
+}
+

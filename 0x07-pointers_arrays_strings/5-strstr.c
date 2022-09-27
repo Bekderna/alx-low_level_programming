6f17fbd71d0aa1a87b74b7db6a0076ad089d7d7e
bekderna <beklibya@gmail.com>
2022-09-27 13:33:40 -0700

    new

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  *  *_strstr- check the code
+ *   *   @haystack : str
+ *    *   @needle : str
+ *      *
+ *       *    * Return: void
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j, x;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		x = 0
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+				x++;
+		}
+		if (x = (j - 1)
+				return (haystack + i - x);
+	}
+	return (0);
+}
+

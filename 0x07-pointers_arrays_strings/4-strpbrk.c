4bc268123cde24a390b8baa429c0a9715eab4f82
bekderna <beklibya@gmail.com>
2022-09-27 13:09:33 -0700

    new

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *  *  *_strpbrk- check the code
+ *   *   @accept : str
+ *    *   @s : str
+ *      *
+ *       *    * Return: void
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+	}
+	if (s[i] != accept[j])
+		return (0);
+	else
+		return (s[i]);
+}
+

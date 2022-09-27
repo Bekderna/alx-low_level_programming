13b9c7ecfc335a7b173c35679d01d9ac9d64f28b
bekderna <beklibya@gmail.com>
2022-09-27 12:09:13 -0700

    new

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  *  *_strchr- check the code
+ *   *   @c : str
+ *    *   @s : str
+ *      *
+ *       *    * Return: void
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+	char d;
+
+	for (i = 0; s[i] != '\0'; i++)
+		d[i] = s[i];
+		if (s[i] == c)
+			break;
+	return (d);
+}
+

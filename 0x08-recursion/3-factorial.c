c1c5f92e7e329f520eaff10826b10868f90b0d68
bekderna <beklibya@gmail.com>
2022-09-28 13:13:35 -0700

    new

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  *  factorial - check the code
+ *   *  @n : srt
+ *      *      *
+ *       *       *    * Return: void
+*/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+		return (0);
+	return (factorial(n - 1) * n);
+}
+

a7fc6724eec461a7637dc965b11b3c7000bc0b8c
bekderna <beklibya@gmail.com>
2022-09-28 12:15:20 -0700

    new

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  *  *_puts_recursion - check the code
+ *   *  @s : srt
+ *      *      *
+ *       *       *    * Return: void
+*/
+void _puts_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+		_putchar(s[i]);
+
+}
+

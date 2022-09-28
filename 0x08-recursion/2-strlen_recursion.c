cf38d2b33576f928568b870f8d6dd900752a5e77
bekderna <beklibya@gmail.com>
2022-09-28 13:00:41 -0700

    new

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  *  _strlen_recursion - check the code
+ *   *  @s : srt
+ *      *      *
+ *       *       *    * Return: void
+*/
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i++
+	if (*s == '\0')
+	{
+		return (i);
+	}
+	_strlen_recursion(*s + 1)
+	
+}
+

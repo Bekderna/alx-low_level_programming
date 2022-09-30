1fe55b98495c3e3ae7487bf0e65a1a2925267dec
bekderna <beklibya@gmail.com>
2022-09-30 12:40:10 -0700

    n

---
+++ 0x0A-argc_argv/0-whatmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print name
+ * argc :fed
+ * argv : fwd
+ * retur value : void
+
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
